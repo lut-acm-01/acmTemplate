a218babbf77466bbd54972e13f38b172e2b1a2ce
WYCTSTF <996098413@qq.com>
2023-06-12 05:33:10 +0800

    赛时header file

--- contest.cpp
+++ contest.cpp
@@ -10,8 +10,8 @@ using i64 = long long;
 using u64 = unsigned long long;
 using u32 = unsigned int;
 using db = double;
-
-using std::cin, std::cout;
+using VI = std::vector<int>;
+using PII = std::pair<int, int>;
 
 #define rep(i,a,n) for(int i=a;i<=n;i++)
 #define per(i,a,n) for(int i=n;i>=a;i--)
@@ -21,7 +21,5 @@ using std::cin, std::cout;
 #define pi acos(-1)
 #define sz(x) ((int)(x).size())
 #define all(x) x.begin(),x.end()
-typedef std::vector<int> VI;
-typedef std::pair<int,int> PII;
 
 i64 gcd(i64 a,i64 b) { return b?gcd(b,a%b):a; }
