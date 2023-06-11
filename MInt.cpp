df554e7ea47ac6289ac459e3b37c984045070368
WYCTSTF <996098413@qq.com>
2023-06-12 05:44:21 +0800

    Mod Int from Jiangly

--- MInt.cpp
+++ MInt.cpp
@@ -1,5 +1,4 @@
-template<class T>
-constexpr T power(T a, i64 b) {
+template <class T> constexpr T power(T a, i64 b) {
   T res = 1;
   for (; b; b /= 2, a *= a) {
     if (b % 2) {
@@ -17,8 +16,7 @@ constexpr i64 mul(i64 a, i64 b, i64 p) {
   }
   return res;
 }
-template<i64 P>
-struct MLong {
+template <i64 P> struct MLong {
   i64 x;
   constexpr MLong() : x{} {}
   constexpr MLong(i64 x) : x{norm(x % getMod())} {}
@@ -31,9 +29,7 @@ struct MLong {
       return Mod;
     }
   }
-  constexpr static void setMod(i64 Mod_) {
-    Mod = Mod_;
-  }
+  constexpr static void setMod(i64 Mod_) { Mod = Mod_; }
   constexpr i64 norm(i64 x) const {
     if (x < 0) {
       x += getMod();
@@ -43,12 +39,8 @@ struct MLong {
     }
     return x;
   }
-  constexpr i64 val() const {
-    return x;
-  }
-  explicit constexpr operator i64() const {
-    return x;
-  }
+  constexpr i64 val() const { return x; }
+  explicit constexpr operator i64() const { return x; }
   constexpr MLong operator-() const {
     MLong res;
     res.x = norm(getMod() - x);
@@ -70,9 +62,7 @@ struct MLong {
     x = norm(x - rhs.x);
     return *this;
   }
-  constexpr MLong &operator/=(MLong rhs) & {
-    return *this *= rhs.inv();
-  }
+  constexpr MLong &operator/=(MLong rhs) & { return *this *= rhs.inv(); }
   friend constexpr MLong operator*(MLong lhs, MLong rhs) {
     MLong res = lhs;
     res *= rhs;
@@ -110,11 +100,9 @@ struct MLong {
   }
 };
 
-template<>
-i64 MLong<0LL>::Mod = 1;
+template <> i64 MLong<0LL>::Mod = 1;
 
-template<int P>
-struct MInt {
+template <int P> struct MInt {
   int x;
   constexpr MInt() : x{} {}
   constexpr MInt(i64 x) : x{norm(x % getMod())} {}
@@ -127,9 +115,7 @@ struct MInt {
       return Mod;
     }
   }
-  constexpr static void setMod(int Mod_) {
-    Mod = Mod_;
-  }
+  constexpr static void setMod(int Mod_) { Mod = Mod_; }
   constexpr int norm(int x) const {
     if (x < 0) {
       x += getMod();
@@ -139,12 +125,8 @@ struct MInt {
     }
     return x;
   }
-  constexpr int val() const {
-    return x;
-  }
-  explicit constexpr operator int() const {
-    return x;
-  }
+  constexpr int val() const { return x; }
+  explicit constexpr operator int() const { return x; }
   constexpr MInt operator-() const {
     MInt res;
     res.x = norm(getMod() - x);
@@ -166,9 +148,7 @@ struct MInt {
     x = norm(x - rhs.x);
     return *this;
   }
-  constexpr MInt &operator/=(MInt rhs) & {
-    return *this *= rhs.inv();
-  }
+  constexpr MInt &operator/=(MInt rhs) & { return *this *= rhs.inv(); }
   friend constexpr MInt operator*(MInt lhs, MInt rhs) {
     MInt res = lhs;
     res *= rhs;
@@ -206,11 +186,9 @@ struct MInt {
   }
 };
 
-template<>
-int MInt<0>::Mod = 1;
+template <> int MInt<0>::Mod = 1;
 
-template<int V, int P>
-constexpr MInt<P> CInv = MInt<P>(V).inv();
+template <int V, int P> constexpr MInt<P> CInv = MInt<P>(V).inv();
 
-constexpr int P = 998244353;
+constexpr int P = 1000000007;
 using Z = MInt<P>;
